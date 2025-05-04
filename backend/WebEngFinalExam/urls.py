5c63fad8b3b9463890b2a15d0b4f3f22ae1bac2a
Daniel Alexis Cruz <dcruz@up.edu.ph>
2025-05-04 15:16:20 +0800

    Appended a catch-all url pattern at the backend project's urls.py

--- backend/WebEngFinalExam/urls.py
+++ backend/WebEngFinalExam/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
+]
+
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(
+        template_name='static/index.html'
+    ), name='spa'),
 ]
